6f969754aadad9a32436a7010a04fc29c0549eb0
pedrolustosab <pedrolustosab@gmail.com>
2024-02-23 21:00:02 -0300

    Final

---
+++ Projeto_tecnicas_programacao/credenciais_db.py
@@ -0,0 +1,5 @@
+db_user = 'admin'
+db_password = 'kRMcZwdRpmoixv1Ugh27EqHrE2g1zmVK'
+db_host = 'dpg-cln5fl8apebc73f54ptg-a.oregon-postgres.render.com'
+db_name = 'vemsertech_72uk'
+
--- Projeto_tecnicas_programacao/otimizador.py
+++ Projeto_tecnicas_programacao/otimizador.py
@@ -1,54 +1,49 @@
 import math
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("lat_lon_escolas.csv")
-pontos_entrega = list(zip(df.lat, df.long))
-
 
 def distance(point1, point2):
     """
     Calcula a distância entre dois pontos geográficos, usando a fórmula de Haversine.
     """
-    lat1, lon1 = point1
-    lat2, lon2 = point2
-    R = 6371  # raio da Terra em quilômetros
-    phi1 = math.radians(lat1)
-    phi2 = math.radians(lat2)
-    delta_phi = math.radians(lat2 - lat1)
-    delta_lambda = math.radians(lon2 - lon1)
+    try:
+        lat1, lon1 = point1
+        lat2, lon2 = point2
+        R = 6371  # raio da Terra em quilômetros
+        phi1 = math.radians(lat1)
+        phi2 = math.radians(lat2)
+        delta_phi = math.radians(lat2 - lat1)
+        delta_lambda = math.radians(lon2 - lon1)
 
-    a = (
-        math.sin(delta_phi / 2) ** 2
-        + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
-    )
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        a = (
+            math.sin(delta_phi / 2) ** 2
+            + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
+        )
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-    d = R * c
-    return d
+        d = R * c
+        print("Distância: {:.4f} km".format(d))
+        return d
+    except Exception as e:
+        print(f"Ocorreu um erro ao calcular a distância: {e}")
 
 
 def nearest_neighbor(points):
-    unvisited = set(
-        range(1, len(points))
-    )  # índices que representam os pontos não visitados
-    current_point = 0
-    route = [current_point]
+    try:
+        unvisited = set(
+            range(1, len(points))
+        )  # índices que representam os pontos não visitados
+        current_point = 0
+        route = [current_point]
 
-    while unvisited:
-        nearest = min(
-            unvisited, key=lambda x: distance(points[current_point], points[x])
-        )
-        route.append(nearest)
-        unvisited.remove(nearest)
-        current_point = nearest
+        while unvisited:
+            nearest = min(
+                unvisited, key=lambda x: distance(points[current_point], points[x])
+            )
+            route.append(nearest)
+            unvisited.remove(nearest)
+            current_point = nearest
 
-    route.append(0)  # Volta ao ponto inicial para fechar o ciclo
-    return route
-
-
-# Exemplo de lista de pontos (latitude, longitude)
-# pontos_entrega = [(lat1, lon1), (lat2, lon2), ...]  # Substitua pelos seus pontos
-
-best_route = nearest_neighbor(pontos_entrega)
-print("Melhor rota:", best_route)
+        route.append(0)  # Volta ao ponto inicial para fechar o ciclo
+        print("Caminho mais curto está traçado")
+        return route
+    except Exception as e:
+        print(f"Ocorreu um erro ao traçar o caminho mais curto: {e}")
